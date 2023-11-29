7efa660726772b09f7895a5c458ccffd07261cca
Kevin D. Mortensen <kevi5221@edu.zealand.dk>
2023-11-29 10:48:50 +0100

    Post function til generer pokemon
    Co-Authored-By: Narenboi <73293318+Narenboi@users.noreply.github.com>

--- routes/Generate.js
+++ routes/Generate.js
@@ -9,6 +9,26 @@ router.get('/', function(req, res, next) {
 
 });
 
+/* GET home page. */
+router.post('/', function(req, res, next) {
+  console.log(req.body); 
+  let Mega = req.body.Mega || null;
+  let Gmax = req.body.Gmax || null;
+  let breedable = !req.body.Unbreedable || null;
+  let Evolved = req.body.Evolved || null;
+
+  console.log(Gmax,Mega,Unbreedable,Evolved,req.body.slider);
+  pokemonListe.generateRandomPokemons(Gmax,Mega,Unbreedable,Evolved,req.body.slider)
+  .then((result) => {
+    res.render('Generate.pug', { title: 'Generate Pokemons', pokemons: result });
+  }).catch((err) => {
+    console.log(err);
+    res.render('Generate.pug', { title: 'Generate Pokemons' });
+  })
+
+});
+
+
 
 
 module.exports = router;

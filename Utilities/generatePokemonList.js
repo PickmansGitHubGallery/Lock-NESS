147d6bbf1355710a6a31af2afb4e4e6bcd125a91
emil51z8 <emil51z8@edu.zealand.dk>
2023-11-29 12:53:56 +0100

    Filterfunktion til generering
    Vi har lavet en metode til at sortere og filtrere pokemons fra databasen

--- Utilities/generatePokemonList.js
+++ Utilities/generatePokemonList.js
@@ -8,21 +8,21 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function generateRandomPokemons(number){
-  db.getAllPokemons()
-.then((pokemons) => {
+function generateRandomPokemons(number, Gmax, Mega, Breedable, Basic){
+  filterPokemons(Gmax, Mega, Breedable, Basic)
+.then((filteredPokemons) => {
   
   let genereretPokemoner = [];
   let randomNumberList = [];
   for(let i = 0; i< number; i++){
-    let randomNumber = getRandomInt(1, 534);
+    let randomNumber = getRandomInt(1, filteredPokemons.length);
     if(randomNumberList.includes(randomNumber)){
       i--;
       console.log(i + 'if statement');
     }
     else{ 
     randomNumberList.push(randomNumber);
-    genereretPokemoner.push(pokemons[randomNumber]);
+    genereretPokemoner.push(filteredPokemons[randomNumber]);
     console.log(i + 'else statement');
     }
 
@@ -32,6 +32,45 @@ function generateRandomPokemons(number){
 })
 }
 
+function filterPokemons(Gmax, Mega, Breedable, Basic) {
+  return new Promise((resolve, reject) => {
+    db.getAllPokemons()
+      .then((pokemons) => {
+        let filteredPokemons = [];
+
+        pokemons.forEach((pokemon) => {
+          if (pokemon.Basic == 1 && pokemon.Breedable == 1) {
+            filteredPokemons.push(pokemon);
+          } else if (Mega == 1 && pokemon.Mega == 1) {
+            filteredPokemons.push(pokemon);
+          } else if (Gmax == 1 && pokemon.Gmax == 1) {
+            filteredPokemons.push(pokemon);
+          } else if (
+            Breedable == 0 &&
+            pokemon.Breedable == 0 &&
+            pokemon.Gmax == 0 &&
+            pokemon.Mega == 0
+          ) {
+            filteredPokemons.push(pokemon);
+          } else if (
+            Basic == 0 &&
+            pokemon.Basic == 0 &&
+            pokemon.Gmax == 0 &&
+            pokemon.Mega == 0
+          ) {
+            filteredPokemons.push(pokemon);
+          }
+        });
+        resolve(filteredPokemons);
+      })
+      .catch((error) => {
+        console.error('Error getting Pokémon data:', error);
+        reject(error);
+      });
+  });
+}
+
 module.exports = {
-  generateRandomPokemons
+  generateRandomPokemons,
+  filterPokemons
 }

c11514789f471aaa0679f406dbfe75985049e1a0
Kevin D. Mortensen <kevi5221@edu.zealand.dk>
2023-12-04 09:49:05 +0100

    Login start

---
+++ database/login.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto');
+
+function generateHashToken(input, salt) {
+    const hash = crypto.createHash('sha256');
+    hash.update(input+ salt);
+    return hash.digest('hex');
+}
+function hashPassword(password) {
+    const hash = crypto.createHash('sha256');
+    hash.update(password);
+    return hash.digest('hex');
+}
+
+  function getCurrentTimestamp() {
+      const currentDateTime = new Date();
+      // Format the date and time as a string (e.g., "2023-11-13 12:34:56")
+      const formattedDateTime = currentDateTime.toISOString().replace('T', ' ').slice(0, 19);
+    
+      return formattedDateTime;
+}
